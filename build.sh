27c74148b24f3c5d0e9d9998f522e3872ab04869
yunser <>
2019-07-04 01:32:05 +0800

    backup

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@ npm run build:commonjs
 npm run build:esm
 npm run copy-css:lib
 npm run copy-css:esm
+
+npm config set registry https://registry.npm.taobao.org
+
+npm config set registry http://registry.npmjs.org
